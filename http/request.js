0c1ccedf19cfe3e41881b52937ecbc7c3b51dcef
murphywuwu <murphywuwu@gmail.com>
2021-01-05 23:50:17 +0800

    initial commit

---
+++ http/request.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const querystring = require('querystring');
+const Buffer = require('buffer').Buffer;
+
+const postData = querystring.stringify({
+  'msg': 'Hello World!'
+});
+
+const options = {
+  hostname: 'www.google.com',
+  port: 80,
+  path: '/upload',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': Buffer.byteLength(postData)
+  }
+}
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);z
+  res.setEncoding('utf-8');
+
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+
+  res.on('end', () => {
+    console.log('No more data in response')
+  });
+});
+
+req.on('error', (e) => {
+  console.log(`problem with request: ${e.message}`);
+})
+
+req.write(postData);
+req.end();
